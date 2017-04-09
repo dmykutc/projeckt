ee4cf3d039f1a31c3ab44155d113e5ae9a2bdc37
Dima <dimaka888@yahoo.com>
2017-04-09 14:28:43 +0000

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+
+$(function(){
+   $(".btm-submit").click(sendForm);
+});
+
+
+function sendForm(e){
+e.preventDefault();
+
+$.ajax({
+    url: "https://formspree.io/dimaka888@yahoo.com", 
+    method: "POST",
+    data: {
+    clientName: $("#text").val(),
+    clientEmail:$("#email").val(),
+    comment:$("#message").val()
+    },
+    dataType: "json"
+}).done(function(){
+    $("#done").html("thank you for contacting us");
+}
+);
+    
+}
