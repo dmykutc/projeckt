0219b3b52719e30b96f43512e3b2f0a8d8869412
Dima <dimaka888@yahoo.com>
2017-04-10 00:39:54 +0000

    initial

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-
-$(function(){
-   $(".btm-submit").click(sendForm);
+$(function() {
+    $(".btm-submit").click(sendForm);
 });
 
 
-function sendForm(e){
-e.preventDefault();
+function sendForm(e) {
+    e.preventDefault();
 
-$.ajax({
-    url: "https://formspree.io/dimaka888@yahoo.com", 
-    method: "POST",
-    data: {
-    clientName: $("#text").val(),
-    clientEmail:$("#email").val(),
-    comment:$("#message").val()
-    },
-    dataType: "json"
-}).done(function(){
-    $("#done").html("Thank you for contacting us");
+    $.ajax({
+        url: "https://formspree.io/dimaka888@yahoo.com",
+        method: "POST",
+        data: {
+            clientName: $("#text").val(),
+            clientEmail: $("#email").val(),
+            comment: $("#message").val()
+        },
+        dataType: "json"
+    }).done(function() {
+        $("#done").html("Thank you for contacting us");
+    });
+
 }
-);
-    
-}
-
-
-
-
-
-
