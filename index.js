92913e9f63231469e4add1abb327e032b25b4c94
Dima <dimaka888@yahoo.com>
2017-04-13 16:37:36 +0000

    initial

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-$(function() {
-    $(".btm-submit").click(sendForm);
-});
+document.querySelector("input[type=submit]").addEventListener("click", sendForm);
 
-
-function sendForm(e) {
-    e.preventDefault();
-
-    $.ajax({
-        url: "https://formspree.io/dimaka888@yahoo.com",
-        method: "POST",
-        data: {
-            clientName: $("#text").val(),
-            clientEmail: $("#email").val(),
-            comment: $("#message").val()
-        },
-        dataType: "json"
-    }).done(function() {
-        $("#done").html("Thank you for contacting us");
-    });
-
-}
-
-
-document.querySelector("input[type=submit]").addEventListener("click",sendForm);
 function sendForm(e) {
     var form = document.querySelector("form");
-    if(form.checkValidity()){
+    if (form.checkValidity()) {
         e.preventDefault();
-        alert("Write code to send form here");
+        $.ajax({
+            url: "https://formspree.io/dimaka888@yahoo.com",
+            method: "POST",
+            data: {
+                clientName: $("#text").val(),
+                clientEmail: $("#email").val(),
+                comment: $("#message").val()
+            },
+            dataType: "json"
+        });
+
     }
 }
-
-
-
-
